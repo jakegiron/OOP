a856dddfd65f7541ce00803f9fcfc3e4c0aa9ab1
jakegiron <giron.jacobo@gmail.com>
2016-02-08 20:57:28 -0700

    Revert "second commit"
    This reverts commit eabf505e59ba6526b12f2dbc12c2bfcf43c80e96.

--- Lab6/vectorFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef VECTORFUNCTIONS
-#define VECTORFUNCTIONS
-#include <vector>
-using namespace std;
-	//Function Proto-types
-	void primeSieve(vector<int> &);
-	void movePrimes(const vector<int> &, vector<int> &);
-#endif
